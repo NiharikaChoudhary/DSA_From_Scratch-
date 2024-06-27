48a4397d5e56a4f64bacfa065b8bb7619c4df124
NiharikaChoudhary <88475866+NiharikaChoudhary@users.noreply.github.com>
2024-06-27 14:56:41 +0530

    Create CountDigits.cpp

---
+++ CountDigits.cpp
@@ -0,0 +1,33 @@
+// HackerRank
+
+// Given a number N. Count the number of digits in N which evenly divide N.
+
+// Note :- Evenly divides means whether N is divisible by a digit i.e. leaves a remainder 0 when divided.
+
+// Example 1:
+
+// Input:
+// N = 12
+// Output:
+// 2
+// Explanation:
+// 1, 2 both divide 12 evenly
+
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        int count=0;
+        int num=N;
+        int last=0;
+        while(N>0) {
+            if( N%10!=0){
+                if(num%(N%10)==0){
+                count++;
+                }
+            }
+            N/=10;
+        }
+        return count;
+    }
+};
